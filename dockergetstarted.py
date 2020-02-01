a2d015355e723bf9b830523542314a835fc0cbd2
clementlr <clement.leroux@gmail.com>
2020-02-01 15:50:47 +0100

    adding cuda9 dockerfile + fix flask version

--- dockergetstarted.py
+++ dockergetstarted.py
@@ -43,8 +43,41 @@ docker container start <container-id>
 # connect to a docker container: https://phase2.github.io/devtools/common-tasks/ssh-into-a-container/
 docker exec -it <container-name> /bin/bash
 
+
+
+############ docker images Dockerfile examples ############
+
 # nginx-php app on Ubuntu 16.04 Docker Image
 https://www.howtoforge.com/tutorial/how-to-create-docker-images-with-dockerfile/
 
 # flask app on Ubuntu 16.04 Docker Image
 https://runnable.com/docker/python/dockerize-your-flask-application
+
+# cuda 9.0 base
+https://gitlab.com/nvidia/container-images/cuda/blob/master/dist/ubuntu16.04/9.0/base/Dockerfile
+
+
+############ docker build Images & run Container ############
+
+# docker pull a built image
+docker pull tensorflow/tensorflow:1.12.3-py3
+
+# docker build an image
+docker build -t <image-name> .
+docker build -t nginx_image . # nginx-php app container build
+docker build -t flask-tutorial:latest . # flask app container build
+docker build -t cuda9.0base:latest . # flask app container build
+
+# docker run an image
+docker run -d -v /webroot:/var/www/html -p 9081:80 --name nginx_php_1 nginx_php_image # run nginx-php app container
+docker run -d -p 5000:5000 flask-tutorial # run flask app container
+docker run -d cuda9.0base -p 6000:80
+docker run -d tensorflow/tensorflow  
+
+############ operation on container ############
+# container status
+docker ps
+docker container ls -a
+
+# connect to a docker container
+docker exec -it <container-name> /bin/bash
